53662818ceb0ed4569e156898b8133ba3f401876
Rothos <h@rothos.com>
2018-07-29 04:32:06 -0400

    Added more questions, now 25, and 5 categories

--- src/components/DeckComponent.js
+++ src/components/DeckComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { categories } from '../data/questions'
+import { toProperCase } from '../utils'
 import Filter from '../containers/Filter'
 
 import Card from '@material-ui/core/Card';
@@ -8,27 +9,37 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const DeckComponent = ({text, category, onClick}) => (
-    <div>
-        <Card>
-            <CardContent>
-                <Typography component="p">
-                    {text}
-                </Typography>
-                <Typography color="textSecondary">
-                    {category}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button onClick={onClick}>Next question</Button>
-            </CardActions>
-        </Card>
+const DeckComponent = ({text, category, onClick}) => {
 
+    return (
+        <div>
+            <Card>
+                <CardContent>
+                    <Typography component="p">
+                        {text}
+                    </Typography>
+                    <Typography color="textSecondary">
+                        {category}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button onClick={onClick}>Next question</Button>
+                </CardActions>
+            </Card>
 
-        <Filter category={categories.GENERIC}>Generic</Filter>
-        <Filter category={categories.THOUGHTFUL}>Thoughtful</Filter>
+            {
+                [...Array(Object.keys(categories).length)].map((x, i) =>
+                    <Filter
+                        category={Object.keys(categories)[i]}
+                        key={i}
+                    >
+                        {toProperCase(Object.keys(categories)[i])}
+                    </Filter>
+                )
+            }
 
-    </div>
-)
+        </div>
+    )
+}
 
 export default DeckComponent
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,8 @@
 
+export function toProperCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
 export function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
